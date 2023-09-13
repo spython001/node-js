f864782bd5ef52d22d571912141e2048c46f2250
spython <emmanuelegbo01@gmail.com>
2023-09-13 13:58:31 +0100

    created a route.js file to house all our route logic

--- app.js
+++ app.js
@@ -20,14 +20,16 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
 
         });
-        req.on('end', () =>{
+        return req.on('end', () =>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFileSync('message.txt', message, (err) => {
+                res.statusCode= 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        res.statusCode= 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
