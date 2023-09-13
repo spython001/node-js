2a7b4302a1d4fc6f776abab03da9463827088844
spython <emmanuelegbo01@gmail.com>
2023-09-13 21:13:05 +0100

    created a new folder routes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
-app.use('/product', (req, res) => {
+app.post('/product', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
