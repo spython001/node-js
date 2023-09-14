337e0a828589a675a0f14620905dfecc62d20f82
spython <emmanuelegbo01@gmail.com>
2023-09-14 01:11:04 +0100

    created a folder util

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
+const path = require('path');
 
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +12,7 @@ app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const path = require('path');
 
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 
