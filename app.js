8796d5996e08a1cd6549779694ce4e44cfd0c1dd
spython <emmanuelegbo01@gmail.com>
2023-09-23 13:01:55 +0100

    created a main-layout pug file to serve as the building block of the page pug files

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoute);
 
 app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(3000, ()=> {
