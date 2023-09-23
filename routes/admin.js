0eac50ebd5d6dcf6eda3aba9b367f97f67edc4fc
spython <emmanuelegbo01@gmail.com>
2023-09-23 14:33:38 +0100

    modified

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const products = [];
 
 // /admin/add-product => GET request
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product'});
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST request
