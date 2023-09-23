7af95a7b9a30ce34512c2db87e66b9037db48105
spython <emmanuelegbo01@gmail.com>
2023-09-23 15:15:21 +0100

    created a add product hbs page

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 const path = require('path');
 const rootDir = require('./util/path')
+const expressHbs = require('express-handlebars');
 
 //USING 'PUG' TEMPLATE ENGINE
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
+//app.set('views', 'views');
+
+//USING 'HANDLEBARS TEMPLATE ENGINE
+app.engine('.hbs', expressHbs.engine({
+    extname: ".hbs",
+    defaultLayout: false
+  }));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 //MIDDLEWARE
