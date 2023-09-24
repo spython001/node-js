b8d94fddc59b279b0f90009b7a14e9676a6d3b2f
spython <emmanuelegbo01@gmail.com>
2023-09-24 08:48:17 +0100

    created a product js file in the model folder

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const shopRoute = require('./routes/shop');
 const path = require('path');
 const rootDir = require('./util/path')
 const expressHbs = require('express-handlebars');
+const errorController = require('./controllers/error');
 
 //USING 'PUG' TEMPLATE ENGINE
 app.set('view engine', 'pug');
@@ -31,9 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoute);
 
-app.use('/', (req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use('/', errorController.get404Page);
 
 app.listen(3000, ()=> {
     console.log('Backend Server is running ...')
