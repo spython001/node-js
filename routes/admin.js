6fb5417b182e399fb23f595173db9063eec4fe5e
spython <emmanuelegbo01@gmail.com>
2023-09-24 13:22:53 +0100

    renamed products js to shop js controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
 const rootDir = require('../util/path');
-const productsController = require('../controllers/products');
+const productsController = require('../controllers/shop');
 
 // /admin/add-product => GET request
 router.get('/add-product', productsController.getAddProduct);
+
+// /admin/products => GET request
+router.get('/products');
 
 // /admin/add-product => POST request
 router.post('/add-product', productsController.postAddProduct);
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,16 @@ const router = require('express').Router();
 const path = require('path');
 const rootDir = require('../util/path');
 const adminData = require('./admin');
-const productController = require('../controllers/products')
+const productController = require('../controllers/shop')
 
 
 router.get('/', productController.getProducts);
+
+router.get('/products');
+
+router.get('/cart');
+
+router.get('./checkout');
 
 
 module.exports = router;
